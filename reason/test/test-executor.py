abaabc51c731c9952a03e4c47b07f1484c3048c8
dali <dali@cs.umu.se>
2023-03-27 09:06:32 +0000

    Added test from original CLEVR, multi and single hop, and moved all test data to its own directory

--- reason/test/test-executor.py
+++ reason/test/test-executor.py
@@ -1,31 +1,50 @@
 import unittest
 from executors import ClevrExecutor
 
-
-class TestStringMethods(unittest.TestCase):
+class TestOriginalClevr(unittest.TestCase):
+    data_path = 'data'
 
     def setUp(self):
-        scene_json = 'val-scenes.json'
-        vocab_json = 'vocab.json'
+        scene_json = '{}/val-scenes.json'.format(self.data_path)
+        vocab_json = '{}/vocab-orig.json'.format(self.data_path)
+        self.executor = ClevrExecutor(scene_json, scene_json, vocab_json)
+
+    def test_same_shape(self):
+        # exist, same_shape, unique, filter_material[metal],
+        # filter_size[large], scene
+        x = [6, 24, 28, 11, 16, 26, 2]
+        index = 0
+        split = 'val'
+        ans = self.executor.run(x,index,split)
+        # if ans == 'error':
+        #     print(self.executor.exe_trace)
+        self.assertEquals(ans, 'no')
+
+class TestMultihop(unittest.TestCase):
+    data_path = 'data'
+
+    def setUp(self):
+        scene_json = '{}/val-scenes.json'.format(self.data_path)
+        vocab_json = '{}/vocab.json'.format(self.data_path)
         self.executor = ClevrExecutor(scene_json, scene_json, vocab_json)
 
     def test_execute_onehop(self):
-        # count, subtraction_set, filter_shape[cube], filter_color[cyan],
-        # subtraction_set, filter_shape[sphere], filter_color[gray], scene, end
+        # count, subtraction_set, filter_shape[cube],
+        # filter_color[gray], scene, end
         x = [4, 17, 13, 8, 16, 2]
         index = 0
         split = 'val'
         ans = self.executor.run(x,index,split)
-        self.assertEquals(ans, '5')
+        self.assertEquals(ans, '3')
 
     def test_execute_subtract_multihop(self):
-        # count, subtraction_set, filter_shape[cube], filter_color[cyan],
-        # subtraction_set, filter_shape[sphere], filter_color[gray], scene, end
-        x = [4, 17, 13, 7, 17, 13, 8, 16, 2]
+        # count, subtraction_set, filter_shape[cylinder], filter_color[brown],
+        # subtraction_set, filter_shape[cube], filter_color[gray], scene, end
+        x = [4, 17, 14, 6, 17, 13, 8, 16, 2]
         index = 0
         split = 'val'
         ans = self.executor.run(x,index,split)
-        self.assertEquals(ans, '5')
+        self.assertEquals(ans, '2')
 
     def test_subtract_empty_set(self):
         # count, subtraction_set, filter_shape[cube], filter_color[cyan],
@@ -35,6 +54,7 @@ class TestStringMethods(unittest.TestCase):
         split = 'val'
         ans = self.executor.run(x,index,split)
         self.assertEquals(ans, '5')
+
 
 
 
